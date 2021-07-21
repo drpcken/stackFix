4001d8f890900be9b4716ba4e49cbbdd227a21dd
drpcken <drpcken@gmail.com>
2021-07-21 16:17:08 -0500

    Create ProductsList.vue

---
+++ ProductsList.vue
@@ -0,0 +1,31 @@
+<template>
+  ...
+   <Column field="id" headerStyle="width: 5%">
+     <template #body="slotProps">
+        <ProductForm :product="slotProps.data" :show="showModal(slotProps.data.id)" />
+           <a href="#" @click.stop="toggleModal(slotProps.data.id)">
+              <span class="pi pi-external-link"> </span>
+           </a>
+     </template>
+   </Column>
+  ...
+</template>
+
+<script>
+import ProductForm from "./forms/ProductForm";
+
+export default {
+  components: { ProductForm },
+  methods: {
+    toggleModal: function (id) {
+      if (this.activeModal !== 0) {
+        this.activeModal = 0;
+        return false;
+      }
+      this.activeModal = id;
+    },
+    showModal: function (id) {
+      return this.activeModal === id;
+    },
+  },
+</script>
