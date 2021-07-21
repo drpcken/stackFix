8465ca9595c6397a5656a17e46160e99f729902d
drpcken <drpcken@gmail.com>
2021-07-21 16:18:39 -0500

    Create BaseModal.vue

---
+++ BaseModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Dialog :header="header" :visible.sync="show" :modal="true" :closable="true" @hide="doit">
+      <slot />
+    </Dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    show: Boolean,
+    header: String,
+  },
+  methods: {
+    doit: function () {
+      let currentShow = this.show;
+      this.$emit("showModel", currentShow)
+    },
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
