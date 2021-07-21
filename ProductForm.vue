9cca36a6b91437dfd56b5234a2e80524e00c4bf6
drpcken <drpcken@gmail.com>
2021-07-21 16:17:57 -0500

    Create ProductForm.vue

---
+++ ProductForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <BaseModal :show="show" :header="product.name">
+      <span class="p-float-label">
+        <InputText id="name" type="text" :value="product.name" />
+        <label for="name">Product</label>
+      </span>
+    </BaseModal>
+  </div>
+</template>
+
+<script>
+import BaseModal from "../_modals/BaseModal";
+
+export default {
+  props: ["product", "show"],
+  components: { BaseModal },
+  data() {
+    return {};
+  },
+};
+</script>
